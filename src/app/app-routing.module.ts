8cfd0937a5163d1ba9d245bb08f401eb34eddf78
beshoyromany <beshoyromany@live.com>
2022-10-08 19:40:14 +0200

    Currency Exchanger "HomePage" Layout - FF1 & FF2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConvertedCurrencyDetailsComponent } from './components/currency-exchanger/components/converted-currency-details/converted-currency-details.component';
+import { CurrencyExchangerComponent } from './components/currency-exchanger/currency-exchanger.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CurrencyExchangerComponent },
+  { path: 'CurrencyExchangerDetails', component: ConvertedCurrencyDetailsComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
